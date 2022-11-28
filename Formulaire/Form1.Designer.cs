8dba4c67915508fdf96628788d454ffeaa874484
Chaymae Mechouahi <113052518+chaymaeme@users.noreply.github.com>
2022-11-28 20:42:57 +0000

    o

--- Formulaire/Form1.Designer.cs
+++ Formulaire/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Formulaire
 {
-    partial class Form1
+    partial class lbl_nom
     {
         /// <summary>
         /// Required designer variable.
@@ -28,20 +28,137 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btn_Ajout = new System.Windows.Forms.Button();
+            this.btn_modif = new System.Windows.Forms.Button();
+            this.btn_supprimer = new System.Windows.Forms.Button();
+            this.btn_annuler = new System.Windows.Forms.Button();
+            this.btn_save = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.recherche = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
-            // Form1
+            // btn_Ajout
+            // 
+            this.btn_Ajout.Location = new System.Drawing.Point(12, 221);
+            this.btn_Ajout.Name = "btn_Ajout";
+            this.btn_Ajout.Size = new System.Drawing.Size(63, 34);
+            this.btn_Ajout.TabIndex = 0;
+            this.btn_Ajout.Text = "Ajouter";
+            this.btn_Ajout.UseVisualStyleBackColor = true;
+            // 
+            // btn_modif
+            // 
+            this.btn_modif.Location = new System.Drawing.Point(81, 222);
+            this.btn_modif.Name = "btn_modif";
+            this.btn_modif.Size = new System.Drawing.Size(63, 33);
+            this.btn_modif.TabIndex = 1;
+            this.btn_modif.Text = "modifier";
+            this.btn_modif.UseVisualStyleBackColor = true;
+            // 
+            // btn_supprimer
+            // 
+            this.btn_supprimer.Location = new System.Drawing.Point(148, 222);
+            this.btn_supprimer.Name = "btn_supprimer";
+            this.btn_supprimer.Size = new System.Drawing.Size(65, 33);
+            this.btn_supprimer.TabIndex = 2;
+            this.btn_supprimer.Text = "supprimer";
+            this.btn_supprimer.UseVisualStyleBackColor = true;
+            // 
+            // btn_annuler
+            // 
+            this.btn_annuler.Location = new System.Drawing.Point(36, 261);
+            this.btn_annuler.Name = "btn_annuler";
+            this.btn_annuler.Size = new System.Drawing.Size(65, 33);
+            this.btn_annuler.TabIndex = 3;
+            this.btn_annuler.Text = "annuler";
+            this.btn_annuler.UseVisualStyleBackColor = true;
+            // 
+            // btn_save
+            // 
+            this.btn_save.Location = new System.Drawing.Point(107, 262);
+            this.btn_save.Name = "btn_save";
+            this.btn_save.Size = new System.Drawing.Size(70, 32);
+            this.btn_save.TabIndex = 4;
+            this.btn_save.Text = "Enregistrer";
+            this.btn_save.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 183);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(201, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 121);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(201, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Nom:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 157);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Prénom:";
+            // 
+            // recherche
+            // 
+            this.recherche.FormattingEnabled = true;
+            this.recherche.Location = new System.Drawing.Point(12, 55);
+            this.recherche.Name = "recherche";
+            this.recherche.Size = new System.Drawing.Size(201, 21);
+            this.recherche.TabIndex = 9;
+            // 
+            // lbl_nom
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(387, 450);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(247, 308);
+            this.Controls.Add(this.recherche);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btn_save);
+            this.Controls.Add(this.btn_annuler);
+            this.Controls.Add(this.btn_supprimer);
+            this.Controls.Add(this.btn_modif);
+            this.Controls.Add(this.btn_Ajout);
+            this.Name = "lbl_nom";
+            this.Text = "Nom:";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_Ajout;
+        private System.Windows.Forms.Button btn_modif;
+        private System.Windows.Forms.Button btn_supprimer;
+        private System.Windows.Forms.Button btn_annuler;
+        private System.Windows.Forms.Button btn_save;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox recherche;
     }
 }
 
--- Formulaire/Program.cs
+++ Formulaire/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +17,30 @@ namespace Formulaire
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new lbl_nom());
+            string server = "localhost";
+            string data = "tp3java";
+            string username = "root";
+            string password = "";
+            string connect = "SERVER=" + server + ";" + "DATABASE=" + data + ";" + "UID" + username + ";" + "PASSWORD=" + password + ";";
+            MySqlConnection cnx = new MySqlConnection(connect);
+            try
+            {
+                cnx.Open();
+                Console.WriteLine("Connected");
+
+                string query = "SELECT * from Commande";
+                MySqlCommand cmd = new MySqlCommand(query, cnx);
+                MySqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    Console.WriteLine(reader.GetString(0));
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
         }
     }
 }
